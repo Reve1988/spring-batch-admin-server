c92bbaefb811fa8ae83cbf52ec7276470fb96d6a
Reve1988 <kgw1988@naver.com>
2019-03-05 14:50:25 +0900

    [#6]Delete Old Spring Batch Metadata - add SpringBatchMetadataCleanScheduler - add @EnableScheduling

--- src/main/java/kr/revelope/spring/batch/admin/SpringBatchAdminServerApplication.java
+++ src/main/java/kr/revelope/spring/batch/admin/SpringBatchAdminServerApplication.java
@@ -3,8 +3,10 @@ package kr.revelope.spring.batch.admin;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @MapperScan(basePackages = "kr.revelope.spring.batch.admin.mapper")
 public class SpringBatchAdminServerApplication {
 	public static void main(String[] args) {
